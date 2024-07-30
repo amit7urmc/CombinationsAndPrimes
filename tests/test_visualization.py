cc4b9201de3986902595618c58ed66ecabc37d30
Amit Kumar <amit7urmc@gmail.com>
2024-07-30 12:53:19 +0530

    docs of visualizations unit tests

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -3,6 +3,9 @@ from src.visualization import create_all_cases_graph, create_one_case_graph
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def test_visualization_all():
+    """
+    tests create_all_cases_graph
+    """
     create_all_cases_graph([2,3], figure_path=os.path.join(CURRENT_DIR,"figures"))
     assert os.path.isfile(f"{CURRENT_DIR}/figures/2Ck.png")
     assert os.path.isfile(f"{CURRENT_DIR}/figures/3Ck.png")
@@ -10,6 +13,9 @@ def test_visualization_all():
     os.remove(f"{CURRENT_DIR}/figures/3Ck.png")
 
 def test_visualization_one():
+    """
+    tests create_one_case_graph
+    """
     n=25
     create_one_case_graph(n, 1, figure_path=os.path.join(CURRENT_DIR,"figures"))
     create_one_case_graph(n,12, figure_path=os.path.join(CURRENT_DIR,"figures"))
